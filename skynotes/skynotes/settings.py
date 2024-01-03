1a5041eb847a2ba8f63810b8624c58d1b1937d32
style77 <stylek777@gmail.com>
2024-01-03 15:17:02 +0100

    chore(api): throttling

--- skynotes/skynotes/settings.py
+++ skynotes/skynotes/settings.py
@@ -95,6 +95,11 @@ REST_FRAMEWORK = {
         "authorization.authentication.JWTCookiesAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/hour", "user": "1000/hour"}  # TODO Consider
 }
 
 SPECTACULAR_SETTINGS = {
