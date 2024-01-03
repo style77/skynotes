bed4310627331661f8459760e42cfce9cd5995f3
style77 <stylek777@gmail.com>
2024-01-03 16:18:57 +0100

    fix(client): remove unused imports and format code

--- client/src/routes/login.tsx
+++ client/src/routes/login.tsx
@@ -2,12 +2,11 @@ import { LoginForm } from "../components/loginForm"
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
-        <div className="flex justify-center items-center w-full md:w-2/5">
-          
-          <LoginForm></LoginForm>
-        </div>
-        <div className="bg-[url('./images/login-bg.jpg')] md:w-3/5 bg-cover bg-center hidden md:block"></div>
+    <div className="min-h-screen flex flex-col md:flex-row justify-center">
+      <div className="flex justify-center items-center w-full md:w-2/5">
+        <LoginForm />
+      </div>
+      <div className="bg-[url('./images/login-bg.jpg')] md:w-3/5 bg-cover bg-center hidden md:block"></div>
     </div>
   )
 }
