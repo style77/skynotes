1e0b5b69c2e42d5cc3be2bdfccbbb1c772e9f4bb
style77 <stylek777@gmail.com>
2024-01-03 16:38:12 +0100

    chore(client): setup PWA

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,14 @@
 import path from "path"
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    VitePWA()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
