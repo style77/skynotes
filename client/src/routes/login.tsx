3dfabf91bf42f4b256d25542ba869df1709abebe
style77 <stylek777@gmail.com>
2024-01-03 16:11:32 +0100

    fix(client): remove unused imports

--- client/src/routes/login.tsx
+++ client/src/routes/login.tsx
@@ -1,6 +1,3 @@
-import { Label } from "../components/ui/label"
-import { Input } from "../components/ui/input"
-import { Button } from "../components/ui/button"
 import { LoginForm } from "../components/loginForm"
 
 export default function LoginPage() {
