02d0398cc5d1e65f294cf700622f1fdaeae19d24
style77 <stylek777@gmail.com>
2024-01-03 18:13:39 +0100

    refactor(client): remove unused variable

--- client/src/store/services/apiSlice.ts
+++ client/src/store/services/apiSlice.ts
@@ -52,5 +52,5 @@ const baseQueryWithReauth: BaseQueryFn<
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: baseQueryWithReauth,
-  endpoints: (builder) => ({}),
+  endpoints: () => ({}),
 });
