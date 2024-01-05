01187b6fca3eb775eb864583d9b1a92107897c95
style77 <stylek777@gmail.com>
2024-01-05 11:24:49 +0100

    chore(api): celery

---
+++ skynotes/skynotes/celery.py
@@ -0,0 +1,9 @@
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "skynotes.settings")
+
+app = Celery("skynotes")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.autodiscover_tasks()
