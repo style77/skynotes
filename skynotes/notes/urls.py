4dfb7ae63a476bbe33ce5af195b29ff94a64903b
style77 <stylek777@gmail.com>
2024-01-07 01:00:52 +0100

    feat(api): GET and POST files views

--- skynotes/notes/urls.py
+++ skynotes/notes/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from notes.views import FilesListView, FilesGroupedListView
+
+
+urlpatterns = [
+    path("files/", FilesListView.as_view()),
+    path("files/<group>/", FilesGroupedListView.as_view()),
+]
---
+++ skynotes/notes/views.py
@@ -0,0 +1,42 @@
+from rest_framework.views import APIView
+from rest_framework import status, parsers
+from rest_framework.response import Response
+from notes.serializers import FileSerializer
+from notes.models import File, Group
+
+from drf_spectacular.utils import extend_schema
+
+
+class FilesListView(APIView):
+    parser_classes = (parsers.MultiPartParser,)
+
+    @extend_schema(
+        description="Retrieve files without any associated group",
+        responses={200: FileSerializer(many=True)},
+    )
+    def get(self, request, group=None, *args, **kwargs):
+        files = File.objects.filter(owner=request.user, group=group).all()
+        serializer = FileSerializer(files, many=True)
+
+        return Response(serializer.data)
+
+    @extend_schema(
+        description="Upload file",
+        request=FileSerializer,
+        responses={200: FileSerializer},
+    )
+    def post(self, request, *args, **kwargs):
+        serializer = FileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FilesGroupedListView(APIView):
+    @extend_schema(
+        description="Retrieve files with associated groups",
+        responses={200: FileSerializer(many=True)},
+    )
+    def get(self, request, group, *args, **kwargs):
+        return FilesListView.as_view()(request._request, group, *args, **kwargs)
