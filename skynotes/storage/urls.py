fb30905b68ed4095931c2616a583bd8f2cc090a1
style77 <stylek777@gmail.com>
2024-01-10 22:13:10 +0100

    style(api): run formatter

--- skynotes/storage/urls.py
+++ skynotes/storage/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from storage.views import FilesGroupedListView, FilesListView, GroupListCreateView, GroupRetrieveUpdateDestroyView
+from storage.views import (
+    FilesGroupedListView,
+    FilesListView,
+    GroupListCreateView,
+    GroupRetrieveUpdateDestroyView,
+)
 
 urlpatterns = [
     path("files/", FilesListView.as_view()),
     path("files/<group>/", FilesGroupedListView.as_view()),
     path("groups/", GroupListCreateView.as_view()),
-    path('groups/<id>/', GroupRetrieveUpdateDestroyView.as_view()),
+    path("groups/<id>/", GroupRetrieveUpdateDestroyView.as_view()),
 ]
