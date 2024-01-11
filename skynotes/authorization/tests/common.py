057b4b715b132658409d85c81dbce6cf26e5fb9f
style77 <stylek777@gmail.com>
2024-01-11 15:03:56 +0100

    chore(api): force_authenticate with set_cookies

--- skynotes/authorization/tests/common.py
+++ skynotes/authorization/tests/common.py
@@ -9,6 +9,11 @@ def set_credentials(token: str, client: APIClient):
     client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
 
 
+def set_cookies(token: str, client: APIClient):
+    client.cookies["access_token"] = token
+
+
 def force_authenticate(user: User, *, client: APIClient = APIClient()):
     token = RefreshToken.for_user(user)
     set_credentials(token.access_token, client)
+    set_cookies(token.access_token, client)
