efd02581b9ff6028ce6a5f77c08641d7119291c8
style77 <stylek777@gmail.com>
2024-01-11 19:52:35 +0100

    refactor(client): go back button

--- client/src/error-page.tsx
+++ client/src/error-page.tsx
@@ -17,6 +17,10 @@ export default function ErrorPage() {
     errorMessage = 'Unknown error';
   }
 
+  const handleGoBack = () => {
+    window.history.back();
+  };
+
   return (
     <div id="error-page" className="w-full h-screen flex flex-row justify-between">
       <div className="flex flex-col justify-center items-center h-full w-full md:w-1/2">
@@ -24,7 +28,7 @@ export default function ErrorPage() {
           <p className='text-primary font-bold pl-2'>{errorCode} {errorMessage}</p>
           <h1 className="text-8xl">Oops!</h1>
           <p className="text-xl pl-2 pt-5 pb-8 font-light text-slate-500">Sorry, an unexpected error has occurred.</p>
-          <a href="/" className="ml-2 px-5 py-3 bg-primary rounded-sm text-white font-medium">Go back</a>
+          <button onClick={handleGoBack} className="ml-2 px-5 py-3 bg-primary rounded-sm text-white font-medium">Go back</button>
         </div>
       </div>
       <div className="bg-[url('./images/login-bg.jpg')] md:w-3/5 bg-cover bg-center hidden md:block">
