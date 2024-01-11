9ae8ae68a7c5cc01a152fec844890a0df90a2981
style77 <stylek777@gmail.com>
2024-01-11 14:01:58 +0100

    style(api): line too long

--- skynotes/storage/urls.py
+++ skynotes/storage/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path("files/", FilesListView.as_view(), name="files"),
     path("files/<group>/", FilesGroupedListView.as_view(), name="grouped-files"),
     path("groups/", GroupListCreateView.as_view(), name="groups"),
-    path("groups/<id>/", GroupRetrieveUpdateDestroyView.as_view(), name="group-details"),
+    path(
+        "groups/<id>/", GroupRetrieveUpdateDestroyView.as_view(), name="group-details"
+    ),
 ]
