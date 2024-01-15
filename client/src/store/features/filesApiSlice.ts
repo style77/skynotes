1992d187f075bed965cb30001d8c3aeb012530a6
style77 <stylek777@gmail.com>
2024-01-15 22:04:20 +0100

    chore(api): file api slice

---
+++ client/src/store/features/filesApiSlice.ts
@@ -0,0 +1,25 @@
+import { apiSlice } from "../services/apiSlice";
+
+export interface File {
+    id: string;
+    created_at: string;
+    updated_at: string;
+    name: string;
+    group: string;
+    description: string;
+    tags: string[];
+    file: string;
+    thumbnail: string;
+}
+
+const filesApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        retrieveRootFiles: builder.query<File[], void>({
+            query: () => "api/files/",
+        }),
+    }),
+});
+
+export const {
+    useRetrieveRootFilesQuery,
+} = filesApiSlice;
