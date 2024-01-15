55a3885d5cf471c2e11d548572065cea22e804a0
style77 <stylek777@gmail.com>
2024-01-15 21:29:26 +0100

    chore(client): sidebar item component

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -1,48 +1,28 @@
-import { Folder, FolderUp, Files, FolderHeart, FolderLock, Trash2, MessageSquareShare, LogOut } from "lucide-react"
-
+import { Folder, FolderUp, Files, FolderHeart, MessageSquareShare, LogOut } from "lucide-react"
+import { SidebarItem } from "./sidebarItem"
 
 export function Sidebar() {
-        return (
-            <div className="flex flex-col justify-between bg-foreground h-full w-80 text-white">
-                <div className="mt-5 mx-6">
-                    <h1 className="text-3xl font-medium">Skynotes</h1>
-                </div>
-                <div className="space-y-4 mx-6">
-                    <a className="inline-flex gap-3 bg-primary w-full py-4 rounded-3xl">
-                        <Folder className="ml-5"/>
-                        <span>My Cloud</span>
-                    </a>
-                    <a className="inline-flex gap-3 w-full py-4 rounded-3xl">
-                        <FolderUp className="ml-5"/>
-                        <span>Shared</span>
-                    </a>
-                    <a className="inline-flex gap-3 w-full py-4 rounded-3xl">
-                        <Files className="ml-5"/>
-                        <span>All files</span>
-                    </a>
-                    <a className="inline-flex gap-3 w-full py-4 rounded-3xl">
-                        <FolderHeart className="ml-5"/>
-                        <span>Favourites</span>
-                    </a>
-                    <a className="inline-flex gap-3 w-full py-4 rounded-3xl">
-                        <FolderLock className="ml-5"/>
-                        <span>Private files</span>
-                    </a>
-                    <a className="inline-flex gap-3 w-full py-4 rounded-3xl">
-                        <Trash2 className="ml-5"/>
-                        <span>Deleted files</span>
-                    </a>
-                </div>
-                <div className="space-y-4 mx-6 mb-12">
-                    <a className="inline-flex gap-3 w-full py-4 rounded-3xl">
-                        <MessageSquareShare className="ml-5"/>
-                        <span>Help & Support</span>
-                    </a>
-                    <a className="inline-flex gap-3 w-full py-4 rounded-3xl">
-                        <LogOut className="ml-5"/>
-                        <span>Log out</span>
-                    </a>
-                </div>
+    return (
+        <div className="flex flex-col justify-between bg-foreground h-full text-white">
+            <div className="mt-5 mx-6">
+                <h1 className="text-3xl font-medium">Skynotes</h1>
             </div>
+            <div className="space-y-4 mx-6">
+                <SidebarItem name="My Cloud" icon={Folder} href="/dashboard" onClick={() => { }} />
+                <SidebarItem name="Shared" icon={FolderUp} href="/dashboard/shared" onClick={() => { }} />
+                <SidebarItem name="All Files" icon={Files} href="/dashboard/files" onClick={() => { }} />
+                <SidebarItem name="Favourites" icon={FolderHeart} href="/dashboard/favourites" onClick={() => { }} />
+            </div>
+            <div className="space-y-4 mx-6 mb-12">
+                <a className="inline-flex gap-3 w-full py-4 rounded-3xl">
+                    <MessageSquareShare className="ml-5" />
+                    <span>Help & Support</span>
+                </a>
+                <a className="inline-flex gap-3 w-full py-4 rounded-3xl">
+                    <LogOut className="ml-5" />
+                    <span>Log out</span>
+                </a>
+            </div>
+        </div>
     )
 }
