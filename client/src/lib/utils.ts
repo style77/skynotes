372dc1bc31a130abac6293f95547f0c43c1c9f7e
style77 <stylek777@gmail.com>
2024-01-27 02:08:44 +0100

    chore(client): unit parameter in humanFriendlySize function

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -5,9 +5,17 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const humanFriendlySize = (size: number) => {
+export const humanFriendlySize = (size: number, unit?: string) => {
   const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   let l = 0, n = size || 0;
-  while (n >= 1024 && ++l) n = n / 1024;
-  return (n.toFixed(n >= 10 || l < 1 ? 0 : 1) + ' ' + units[l]);
+
+  const unitIndex = unit ? units.indexOf(unit.toUpperCase()) : -1;
+
+  if (unitIndex !== -1) {
+    n = n / Math.pow(1024, unitIndex);
+    return (n.toFixed(n >= 10 || unitIndex < 1 ? 0 : 1) + ' ' + units[unitIndex]);
+  } else {
+    while (n >= 1024 && ++l) n = n / 1024;
+    return (n.toFixed(n >= 10 || l < 1 ? 0 : 1) + ' ' + units[l]);
+  }
 }
