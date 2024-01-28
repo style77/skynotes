637ef6005a05dac90e20f79215e1d484951286b6
style77 <stylek777@gmail.com>
2024-01-28 21:20:29 +0100

    refactor(client): change cloud limit text opacity to be less focused

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -37,7 +37,7 @@ export function Navbar() {
                 !user && (userLoading || userError) ? <Spinner /> : user && (
                     <div className="flex flex-row gap-4 items-center">
                         <div className="border border-gray-200/75 hover:border-gray-400 transition px-4 py-2 rounded-3xl">
-                            <span className="text-sm font-semibold text-primary cursor-default">{humanFriendlySize(user.storage_used, "GB")} / {humanFriendlySize(user.storage_limit, "GB")}</span>
+                            <span className="text-sm font-semibold text-primary/75 hover:text-primary cursor-default">{humanFriendlySize(user.storage_used, "GB")} / {humanFriendlySize(user.storage_limit, "GB")}</span>
                         </div>
                         <div>
                             <Avatar>
