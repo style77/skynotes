9c51333d96a01f5f774a26810bde253e5a3da3ee
style77 <stylek777@gmail.com>
2024-01-28 20:20:26 +0100

    chore(client): Lora font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        lora: ["Lora", "serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
