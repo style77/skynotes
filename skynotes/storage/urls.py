f453b420823847c8d73737740949c7cde2d57f54
style77 <stylek777@gmail.com>
2024-01-29 21:06:47 +0100

    refactor(client): keep urls consistency

--- skynotes/storage/urls.py
+++ skynotes/storage/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path("file/<id>/", FileDetailsView.as_view(), name="file-detail"),
     path("files/", FilesListView.as_view(), name="files"),
     path("files/<group>/", FilesGroupedListView.as_view(), name="grouped-files"),
-    path("groups/", GroupListCreateView.as_view(), name="groups"),
     path(
         "groups/<id>/", GroupRetrieveUpdateDestroyView.as_view(), name="group-details"
     ),
+    path("groups/", GroupListCreateView.as_view(), name="groups"),
 ]
