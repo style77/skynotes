a9c9cc97f8a66453e91fa6966c15aa360b1c1ac8
style77 <stylek777@gmail.com>
2024-02-03 04:01:55 +0100

    refactor(client): move thumbnail in to seperate component

--- client/src/routes/dashboard.tsx
+++ client/src/routes/dashboard.tsx
@@ -27,6 +27,7 @@ import { apiSlice } from "@/store/services/apiSlice";
 import { useAppSelector } from "@/store/hooks";
 
 import { ItemsGrid } from "@/components/items/itemsGrid";
+import { Thumbnail } from "@/components/items/thumbnail";
 
 type FileItemProps = {
   id: string;
@@ -38,47 +39,6 @@ type FileItemProps = {
   onClick?: () => void;
   focused?: boolean;
 }
-
-interface ThumbnailProps {
-  mediaUrl: string;
-}
-
-const Thumbnail: React.FC<ThumbnailProps> = ({ mediaUrl }) => {
-
-  const [thumbnailUrl, setThumbnailUrl] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchThumbnail = async () => {
-      const url = `http://localhost:8000${mediaUrl}?thumbnail=true`
-      try {
-        await fetch(url, {
-          credentials: 'include',
-        });
-        setThumbnailUrl(url);
-      } catch (e) {
-        console.error(e);
-        setThumbnailUrl(null)
-      }
-    }
-
-    fetchThumbnail();
-  }, [mediaUrl]);
-
-  return (
-    <div className="w-full select-none bg-black rounded-t-lg">
-      {
-        thumbnailUrl ? (
-          <div className="relative rounded-t-lg h-24 w-full">
-            <img src={thumbnailUrl} alt={`Thumbnail for media ID ${mediaUrl}`} className="rounded-t-lg h-24 w-full select-none" />
-            <div className="absolute top-0 left-0 right-0 bottom-0 rounded-t-lg bg-gradient-to-b from-black/40 to-black/75"></div>
-          </div>
-        ) : (
-          <div className="w-full h-24 bg-gray-300 rounded-t-lg animate-pulse"></div>
-        )
-      }
-    </div>
-  );
-};
 
 export function FileItem(props: FileItemProps) {
   const formattedDate = format(parseISO(props.createdAt), 'dd/MM/yyyy, hh:mm a');
