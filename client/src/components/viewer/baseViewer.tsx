470f077167c15276c9f21d409d4ff4ae6c8022e6
style77 <stylek777@gmail.com>
2024-02-03 02:55:57 +0100

    chore(client): audio viewer

--- client/src/components/viewer/baseViewer.tsx
+++ client/src/components/viewer/baseViewer.tsx
@@ -1,5 +1,9 @@
 
 import { File } from "@/store/features/filesApiSlice";
+import { Download, Link, MoreHorizontal, Pause, Play, SkipBack, SkipForward, Volume2, VolumeX, X } from "lucide-react";
+import { useRef, useEffect, useState } from "react";
+import { useWavesurfer } from '@wavesurfer/react'
+
 
 type ViewerProps = {
   open: boolean;
@@ -51,23 +55,191 @@ function BaseViewer(props: BaseViewerProps) {
 
   return (
     <>
-      <div className={`fixed inset-0 bg-black bg-opacity-50 z-40 ${props.open ? 'flex' : 'hidden'} justify-center items-center`}>
-        <div className={`bg-white w-[90vw] h-[90vh] max-w-[90vw] max-h-[90vh] rounded-lg overflow-auto z-[9999] ${props.open ? 'block' : 'hidden'} p-4`}>
-          <div className="flex justify-end">
-            <button onClick={handleClose} className="text-2xl font-bold mb-2">&times;</button>
+      <div className={`fixed inset-0 z-40 bg-black text-white justify-between items-center p-4 flex-col h-full w-full ${props.open ? "flex" : "hidden"}`}>
+        <div className="w-full justify-between flex flex-row items-center">
+          <div className="flex flex-row gap-6">
+            <Download className="text-gray-300 hover:text-white cursor-pointer" size={22} />
+            <Link className="text-gray-300 hover:text-white cursor-pointer" size={22} />
+            <MoreHorizontal className="text-gray-300 hover:text-white cursor-pointer" size={22} />
           </div>
-          {props.children}
+          <X className="text-gray-300 hover:text-white cursor-pointer" size={24} onClick={handleClose} />
         </div>
+        {props.children}
       </div>
     </>
   )
 }
 
 function AudioViewer(props: ViewerProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [hoverProgress, setHoverProgress] = useState<number>(0);
+  const progressBarRef = useRef<HTMLDivElement>(null);
+
+  const [volume, setVolume] = useState<number>(0.5);
+  const [muted, setMuted] = useState<boolean>(false);
+  const [showVolumeSlider, setShowVolumeSlider] = useState<boolean>(false);
+  const lastVolumeRef = useRef<number>(volume);
+
+  const { wavesurfer, isPlaying, currentTime } = useWavesurfer({
+    container: containerRef,
+    height: 200,
+    waveColor: '#7c3aed',
+    progressColor: '#6c23eb',
+    cursorColor: '#5f14e0',
+    barWidth: 2,
+    barRadius: 3,
+    normalize: true,
+    interact: false,
+    cursorWidth: 0,
+  });
+
+  useEffect(() => {
+    const fetchAudio = async () => {
+      try {
+        const response = await fetch(`http://localhost:8000${props.file.file}`, {
+          credentials: 'include'
+        });
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const blob = await response.blob();
+        wavesurfer!.loadBlob(blob);
+      } catch (e) {
+        console.error('Failed to fetch audio:', e);
+      }
+    };
+
+    if (wavesurfer) {
+      fetchAudio();
+    }
+  }, [props.file.file, wavesurfer]);
+
+  useEffect(() => {
+    if (wavesurfer) {
+      wavesurfer.setVolume(volume);
+      if (volume === 0) {
+        setMuted(true);
+      } else {
+        setMuted(false);
+      }
+    }
+  }, [volume, wavesurfer]);
+
+  const playHandler = () => {
+    wavesurfer && wavesurfer.playPause()
+  };
+  const skipBackHandler = () => { /* ... */ };
+  const skipForwardHandler = () => { /* ... */ };
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+  }
+
+  const getProgress = () => {
+    if (wavesurfer) {
+      const progress = (wavesurfer.getCurrentTime() / wavesurfer.getDuration()) * 100;
+      return progress;
+    }
+    return 0;
+  }
+
+  const seekAudio = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const rect = progressBarRef.current!.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const totalWidth = rect.width;
+    const percentage = (x / totalWidth) * 100;
+    wavesurfer!.seekTo(percentage / 100);
+  };
+
+  const updateHoverProgress = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const rect = progressBarRef.current!.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const totalWidth = rect.width;
+    const percentage = (x / totalWidth) * 100;
+    setHoverProgress(percentage);
+  };
+
+  const toggleMute = () => {
+    if (wavesurfer) {
+      if (!muted) {
+        lastVolumeRef.current = wavesurfer.getVolume();
+        wavesurfer.setVolume(0);
+        setMuted(true);
+      } else {
+        wavesurfer.setVolume(lastVolumeRef.current);
+        setMuted(false);
+      }
+    }
+  };
+
+  const changeVolume = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newVolume = Number(e.target.value);
+    setVolume(newVolume);
+    if (muted && newVolume > 0) {
+      setMuted(false);
+    }
+  };
+
   return (
     <BaseViewer {...props}>
-      {/* <audio controls src={`http://localhost:8000${props.file.file}`} /> */}
-      <div className="min-w-10/12 min-h-[80%] flex justify-center items-start border-double border-gray-100 border-4 rounded-radius"></div>
+      <div className="relative w-full h-full">
+        <div className="absolute bottom-10 w-full" ref={containerRef} />
+      </div>
+      {
+        wavesurfer && (
+          <div className="flex-grow w-full">
+            <div
+              className="mb-2 relative cursor-pointer"
+              ref={progressBarRef}
+              onClick={seekAudio}
+              onMouseMove={updateHoverProgress}
+              onMouseLeave={() => setHoverProgress(0)}
+            >
+              <div className="h-2 bg-gray-800 rounded-radius">
+                <div className="bg-primary h-2 rounded-radius absolute" style={{ width: `${getProgress()}%` }}></div>
+                <div className="bg-primary/50 h-2 rounded-radius absolute" style={{ width: `${hoverProgress}%` }}></div>
+              </div>
+              <div className="flex justify-between text-xs mt-2">
+                <span>{formatTime(currentTime)}</span>
+                <span>{formatTime(wavesurfer.getDuration())}</span>
+              </div>
+            </div>
+            <div className="flex justify-center items-center gap-4">
+              <SkipBack className="text-gray-400 hover:text-white cursor-pointer" size={24} onClick={skipBackHandler} />
+              {
+                isPlaying ? <Pause className="text-gray-400 hover:text-white cursor-pointer" size={24} onClick={playHandler} /> : <Play className="text-gray-400 hover:text-white cursor-pointer" size={24} onClick={playHandler} />
+              }
+              <SkipForward className="text-gray-400 hover:text-white cursor-pointer" size={24} onClick={skipForwardHandler} />
+              <div
+                className="relative items-center gap-4 flex cursor-pointer"
+                onMouseEnter={() => setShowVolumeSlider(true)}
+                onMouseLeave={() => setShowVolumeSlider(false)}
+              >
+                <button onClick={toggleMute}>
+                  {muted ? (
+                    <VolumeX className="text-gray-400 cursor-pointer" size={24} />
+                  ) : (
+                    <Volume2 className="text-gray-400 cursor-pointer" size={24} />
+                  )}
+                </button>
+                {showVolumeSlider && (
+                  <input
+                    type="range"
+                    min="0"
+                    max="1"
+                    step="0.05"
+                    value={volume}
+                    onChange={changeVolume}
+                    className="absolute bottom-full w-16 cursor-pointer accent-primary"
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+        )
+      }
     </BaseViewer>
   )
 }
