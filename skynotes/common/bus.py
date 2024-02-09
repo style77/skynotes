80ea3450b0d1a46e977d9fcf431f3d11345c5fd5
style77 <stylek777@gmail.com>
2024-02-09 18:22:08 +0100

    refactor(api): event listener registration in EventBus class

--- skynotes/common/bus.py
+++ skynotes/common/bus.py
@@ -9,14 +9,17 @@ class EventBus(metaclass=SingletonMeta):
 
     def listener(self, event_type: str):
         def wrapper(func: Callable):
-            if event_type not in self.listeners:
-                self.listeners[event_type] = []
-            self.listeners[event_type].append(func)
+            self.register_listener(event_type, func)
             return func
-
         return wrapper
 
+    def register_listener(self, event_type: str, func: Callable):
+        if event_type not in self.listeners:
+            self.listeners[event_type] = []
+        self.listeners[event_type].append(func)
+
     def emit(self, event_type: str, *args, **kwargs):
+
         def invoke_listeners():
             if event_type in self.listeners:
                 for listener in self.listeners[event_type]:
