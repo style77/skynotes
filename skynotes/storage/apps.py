901bde59b3b79fdb2886d4498acd43063bb78f42
style77 <stylek777@gmail.com>
2024-02-09 18:23:04 +0100

    chore(api): event listeners for file upload and thumbnail generation to reigster when app is ready

--- skynotes/storage/apps.py
+++ skynotes/storage/apps.py
@@ -4,3 +4,17 @@ from django.apps import AppConfig
 class StorageConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "storage"
+
+    def ready(self) -> None:
+        from common.bus import bus
+        from storage.tasks import (
+            handle_file_upload,
+            handle_thumbnail_generation,
+            wrap_celery_task,
+        )
+
+        bus.register_listener("file:created", wrap_celery_task(handle_file_upload))
+        bus.register_listener(
+            "file:uploaded", wrap_celery_task(handle_thumbnail_generation)
+        )
+        return super().ready()
