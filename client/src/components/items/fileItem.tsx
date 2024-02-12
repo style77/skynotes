ab804bbe88c0fec0cbc3235cc5ff5ddb09b9cc9d
style77 <stylek777@gmail.com>
2024-02-12 15:47:02 +0100

    refactor(client): fileItem component to use File type

--- client/src/components/items/fileItem.tsx
+++ client/src/components/items/fileItem.tsx
@@ -1,4 +1,4 @@
-import { useDeleteFileMutation } from "@/store/features/filesApiSlice";
+import { File, useDeleteFileMutation } from "@/store/features/filesApiSlice";
 import { useState, useEffect } from "react";
 import { format, parseISO } from 'date-fns';
 import { MoreVertical } from "lucide-react";
@@ -21,26 +21,20 @@ import { apiSlice } from "@/store/services/apiSlice";
 import { Thumbnail } from "@/components/items/thumbnail";
 import { BouncingDotsLoader } from "../ui/bouncing-dots";
 import { FileShareModal } from "../file/fileShareModal";
+import { EditFileModal } from "../file/fileModal";
 
 type FileItemProps = {
-  id: string;
-  name: string;
-  file: string;
-  thumbnail: string | null;
-  size: number;
-  tags: string[];
-  createdAt: string;
+  file: File;
   onClick?: () => void;
   focused?: boolean;
 }
 
 export function FileItem(props: FileItemProps) {
-  const formattedDate = format(parseISO(props.createdAt), 'dd/MM/yyyy, hh:mm a');
+  const formattedDate = format(parseISO(props.file.created_at), 'dd/MM/yyyy, hh:mm a');
 
   const [deleteFile] = useDeleteFileMutation()
 
-  // @ts-expect-error TODO: Implement edit file
-  const [editOpen, setEditOpen] = useState(false);  // eslint-disable-line
+  const [editOpen, setEditOpen] = useState(false);
   const [deleteOpen, setDeleteOpen] = useState(false);
   const [shareOpen, setShareOpen] = useState(false);
   const [fileUploadingDeleteOpen, setFileUploadingDeleteOpen] = useState(false);
@@ -69,7 +63,7 @@ export function FileItem(props: FileItemProps) {
   }, [props.file, dispatch]);
 
   const handleDelete = async () => {
-    await deleteFile({ id: props.id }).unwrap()
+    await deleteFile({ id: props.file.id }).unwrap()
     setDeleteOpen(false);
     setFileUploadingDeleteOpen(false)
   }
@@ -95,7 +89,7 @@ export function FileItem(props: FileItemProps) {
                   </>
                 ) : (
                   <>
-                    This action cannot be undone. This will permanently delete the <code>{truncateFileName(props.name)}</code> file
+                    This action cannot be undone. This will permanently delete the <code>{truncateFileName(props.file.name)}</code> file
                     and remove your data from our servers.
                   </>
                 )
@@ -108,7 +102,8 @@ export function FileItem(props: FileItemProps) {
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
-      <FileShareModal open={shareOpen} setOpen={setShareOpen} fileId={props.id} fileName={props.name} />
+      <FileShareModal open={shareOpen} setOpen={setShareOpen} fileId={props.file.id} fileName={props.file.name} />
+      <EditFileModal open={editOpen} setOpen={setEditOpen} file={props.file} />
       <DropdownMenu>
         {
           fileUploaded ? (
@@ -116,18 +111,18 @@ export function FileItem(props: FileItemProps) {
               <div className={`flex flex-col items-center w-full md:w-56 h-56 max-h-56 hover:ring-2 hover:ring-primary/50 rounded-lg cursor-pointer shadow-lg transition-all` + (props.focused && " ring-primary ring-2")} onClick={props.onClick}>
                 <div className="flex flex-col bg-white h-full gap-1 rounded-t-lg w-full">
                   <div className="relative">
-                    <Thumbnail fileName={props.name} mediaUrl={props.thumbnail} />
-                    <DropdownMenuTrigger className={`p-1 ${props.thumbnail ? "hover:bg-gray-100/25" : "hover:bg-gray-300/25"} rounded-full transition inline-flex justify-center items-center z-10 absolute top-3 right-3`}>
-                      <MoreVertical className={props.thumbnail ? "text-gray-50" : "text-gray-600"} />
+                    <Thumbnail fileName={props.file.name} mediaUrl={props.file.thumbnail} />
+                    <DropdownMenuTrigger className={`p-1 ${props.file.thumbnail ? "hover:bg-gray-100/25" : "hover:bg-gray-300/25"} rounded-full transition inline-flex justify-center items-center z-10 absolute top-3 right-3`}>
+                      <MoreVertical className={props.file.thumbnail ? "text-gray-50" : "text-gray-600"} />
                     </DropdownMenuTrigger>
                   </div>
                   <div className="mt-1 px-3 flex flex-col">
-                    <span className="font-semibold text-xs sm:text-xs md:text-base truncate">{props.name}</span>
+                    <span className="font-semibold text-xs sm:text-xs md:text-base truncate">{props.file.name}</span>
                     <span className="opacity-50 text-xs">{formattedDate}</span>
                   </div>
                 </div>
                 <div className="bg-primary text-white rounded-b-lg w-full flex flex-row py-4 px-4">
-                  <span className="text-xs font-semibold">{humanFriendlySize(props.size)}</span>
+                  <span className="text-xs font-semibold">{humanFriendlySize(props.file.size)}</span>
                 </div>
               </div>
               <DropdownMenuContent>
@@ -153,12 +148,12 @@ export function FileItem(props: FileItemProps) {
                     </div>
                   </div>
                   <div className="mt-1 px-3 flex flex-col">
-                    <span className="font-semibold text-xs sm:text-xs md:text-base truncate">{props.name}</span>
+                    <span className="font-semibold text-xs sm:text-xs md:text-base truncate">{props.file.name}</span>
                     <span className="opacity-50 text-xs">{formattedDate}</span>
                   </div>
                 </div>
                 <div className="bg-primary text-white rounded-b-lg w-full flex flex-row py-4 px-4">
-                  <span className="text-xs font-semibold">{props.size && humanFriendlySize(props.size)}</span>
+                  <span className="text-xs font-semibold">{props.file.size && humanFriendlySize(props.file.size)}</span>
                 </div>
               </div>
               <DropdownMenuContent className="z-[100]">
