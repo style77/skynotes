de98973a45f9165f85c3945d72d3547fc6a81208
style77 <stylek777@gmail.com>
2024-02-13 01:24:24 +0100

    chore(client): add type to spoiler component

--- client/src/components/ui/spoiler.tsx
+++ client/src/components/ui/spoiler.tsx
@@ -2,27 +2,28 @@ import { useState } from 'react';
 
 type SpoilerProps = {
     text: string;
+    type: "elipsis" | "blur" | "blackout";
 }
 
 const Spoiler = ({ text }: SpoilerProps) => {
-  const [revealed, setRevealed] = useState(false);
+    const [revealed, setRevealed] = useState(false);
 
-  const handleSpoilerClick = () => {
-    setRevealed(true);
-  };
+    const handleSpoilerClick = () => {
+        setRevealed(true);
+    };
 
-  return (
-    <div className="w-full">
-      {revealed ? (
-        <span>{text}</span>
-      ) : (
-        <span onClick={handleSpoilerClick} className="cursor-pointer w-full bg-black">
-          <span className="text-black">{text.substring(0, 2)}</span>
-          <span className="text-transparent bg-black hover:bg-black/80 transition">{text.substring(2)}</span>
-        </span>
-      )}
-    </div>
-  );
+    return (
+        <div className="w-full">
+            {revealed ? (
+                <code>{text}</code>
+            ) : (
+                <span onClick={handleSpoilerClick} className="cursor-pointer w-full bg-black">
+                    <span className="text-black">{text.substring(0, 2)}</span>
+                    <span className="text-transparent bg-black hover:bg-black/80 transition">{text.substring(2)}</span>
+                </span>
+            )}
+        </div>
+    );
 };
 
 export default Spoiler;
