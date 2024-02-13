b00a451b2187048c27c862a143910d6a98eabd63
style77 <stylek777@gmail.com>
2024-02-13 03:55:10 +0100

    refactor(api): file sharing and analytics endpoints

--- skynotes/storage/views.py
+++ skynotes/storage/views.py
@@ -97,9 +97,7 @@ class FileDetailsView(
                 file__owner=request.user, file__id=pk
             ).all()
             serializer = FileShareSerializer(file_shares, many=True)
-            if serializer.is_valid():
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data)
         else:
             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
@@ -133,9 +131,7 @@ class FileDetailsView(
         analytics = FileAnalyticsService.get_file_analytics(token)
         serializer = FileAnalyticsSerializer(analytics, many=True)
 
-        if serializer.is_valid():
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data)
 
 
 @extend_schema(tags=["files"])
